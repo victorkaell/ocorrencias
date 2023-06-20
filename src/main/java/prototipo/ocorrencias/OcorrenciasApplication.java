e29624a14fb052228b0f243d9362194b2ab62566
Victor Kaell da Cruz Silva <20221204010001@ifrn.local>
2023-06-20 15:52:36 -0300

    Importando a aplicação inicial

---
+++ src/main/java/prototipo/ocorrencias/OcorrenciasApplication.java
@@ -0,0 +1,13 @@
+package prototipo.ocorrencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcorrenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcorrenciasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prototipo/ocorrencias/OcorrenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package prototipo.ocorrencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcorrenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
