e204591a621fc93664c4211f818d228939937afb
Victor Kaell <victorkaell13@gmail.com>
2026-03-28 17:54:07 -0300

    Consertados imports

--- src/main/java/prototipo/ocorrencias/rest/TratamentosRestController.java
+++ src/main/java/prototipo/ocorrencias/rest/TratamentosRestController.java
@@ -8,10 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 import jakarta.validation.Valid;
 import prototipo.ocorrencias.models.Tratamento;
 import prototipo.ocorrencias.repositories.TratamentoRepository;
