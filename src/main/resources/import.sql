745f0fb6070f65a24bd5266efc1bf48baedcd750
Victor Kaell <victorkaell13@gmail.com>
2026-03-27 23:47:33 -0300

    Adicionados dados no banco para testes de uso

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+-- Categorias
+INSERT INTO categoria (id, nome) VALUES (1, 'Aluno');
+INSERT INTO categoria (id, nome) VALUES (2, 'Terceirizado');
+INSERT INTO categoria (id, nome) VALUES (3, 'Servidor');
+
+-- Usuário Teste (matricula: 1234, senha: 1234)
+INSERT INTO usuario (matricula, senha) VALUES (1234, '1234');
