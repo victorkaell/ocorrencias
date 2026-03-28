9309a0974a18ec458137214f1c59490448331d3f
Victor Kaell <victorkaell13@gmail.com>
2026-03-28 16:00:21 -0300

    Adicionadas propriedades de segurança a Entidade

--- src/main/java/prototipo/ocorrencias/models/Usuario.java
+++ src/main/java/prototipo/ocorrencias/models/Usuario.java
@@ -1,5 +1,8 @@
 package prototipo.ocorrencias.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
@@ -15,6 +18,7 @@ public class Usuario {
 	
 	@NotBlank(message = "O campo senha não pode estar em branco.")
 	@Size(min = 4, message = "A senha precisa ter no mínimo 4 caracteres.")
+	@JsonProperty(access = Access.WRITE_ONLY)
 	private String senha;
 
 	public Long getMatricula() {
